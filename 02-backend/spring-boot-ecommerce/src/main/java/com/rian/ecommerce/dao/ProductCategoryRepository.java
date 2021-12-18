0ffbd7c50df2083cab462f542b4e0bac592cd7b3
Rianto <rianto.rnt@gmail.com>
2021-12-18 21:27:11 +0800

    create REST APIs with Spring JPA Repo and Spring Data REST

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/rian/ecommerce/dao/ProductCategoryRepository.java
@@ -0,0 +1,10 @@
+package com.rian.ecommerce.dao;
+
+import com.rian.ecommerce.entity.ProductCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
+public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
+
+}
