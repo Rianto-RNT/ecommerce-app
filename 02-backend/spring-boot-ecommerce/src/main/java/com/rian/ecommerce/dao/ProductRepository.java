6b7a87fbdccd0f15ba6620fcb75a663fe5d3f08a
Rianto <rianto.rnt@gmail.com>
2021-12-19 13:20:12 +0800

    Integrating Frontend and Backend (angular - spring boot)

--- 02-backend/spring-boot-ecommerce/src/main/java/com/rian/ecommerce/dao/ProductRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/rian/ecommerce/dao/ProductRepository.java
@@ -2,6 +2,8 @@ package com.rian.ecommerce.dao;
 
 import com.rian.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
