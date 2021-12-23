2c6f9866912f030e84fcba49c8d494e7e2997335
Rianto <rianto.rnt@gmail.com>
2021-12-23 09:46:11 +0800

    Modifiy Spring Boot app part 2 & Bugs fix

--- 02-backend/spring-boot-ecommerce/src/main/java/com/rian/ecommerce/config/MyDataRestConfig.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/rian/ecommerce/config/MyDataRestConfig.java
@@ -2,15 +2,14 @@ package com.rian.ecommerce.config;
 
 import com.rian.ecommerce.entity.Product;
 import com.rian.ecommerce.entity.ProductCategory;
-import org.hibernate.type.EntityType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
@@ -21,12 +20,12 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
     private EntityManager entityManager;
 
     @Autowired
-    public void myDataRestConfig(EntityManager theEntityManager) {
+    public MyDataRestConfig(EntityManager theEntityManager) {
         entityManager = theEntityManager;
     }
 
-    @Override
-    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+    // @Override <- @TODO fix this error in override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
 
         HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
 
@@ -34,7 +33,7 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
         config.getExposureConfiguration()
                 .forDomainType(Product.class)
                 .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
-                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));        // disable HTTP methods for Product: PUT, POST and DELETE
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
 
         // disable HTTP methods for ProductCategory: PUT, POST and DELETE
         config.getExposureConfiguration()
@@ -42,27 +41,37 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
                 .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
                 .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
 
-        // Call an internal helper method
+        // call an internal helper method
         exposeIds(config);
     }
 
     private void exposeIds(RepositoryRestConfiguration config) {
-        // Expose entity ids
+
+        // expose entity ids
         //
 
-        // - get pf all entity classes from the entity manager
+        // - get a list of all entity classes from the entity manager
         Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
 
-        // - create an array of the entity type
+        // - create an array of the entity types
         List<Class> entityClasses = new ArrayList<>();
 
         // - get the entity types for the entities
         for (EntityType tempEntityType : entities) {
             entityClasses.add(tempEntityType.getJavaType());
+        }
 
-        // - expose the entity ids for the  array of entity/domain types
+        // - expose the entity ids for the array of entity/domain types
         Class[] domainTypes = entityClasses.toArray(new Class[0]);
         config.exposeIdsFor(domainTypes);
-        }
     }
 }
+
+
+
+
+
+
+
+
+
