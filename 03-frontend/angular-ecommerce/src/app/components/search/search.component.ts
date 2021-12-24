ba6c2696226e1569ded0ed5fa4868bc3d07f8588
Rianto <rianto.rnt@gmail.com>
2021-12-24 11:08:43 +0800

    search product by keyword - Send data to search route

--- 03-frontend/angular-ecommerce/src/app/components/search/search.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/search/search.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  doSearch(value: string) {
+    console.log(`value=${value}`);
+    this.router.navigateByUrl(`/search/${value}`);
   }
 
 }
