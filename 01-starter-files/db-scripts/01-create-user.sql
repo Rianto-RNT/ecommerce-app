5e7362dbe9c6aadac18aaf6b64cdfea109b44047
Rianto <rianto.rnt@gmail.com>
2022-01-07 17:34:58 +0800

    Checkout Form - Populate Countries and state - JPA Entities (Backend)

--- 01-starter-files/db-scripts/01-create-user.sql
+++ 01-starter-files/db-scripts/01-create-user.sql
@@ -12,4 +12,4 @@ GRANT ALL PRIVILEGES ON * . * TO 'ecommerceapp'@'localhost';
 # See the MySQL Reference Manual for details: 
 # https://dev.mysql.com/doc/refman/8.0/en/caching-sha2-pluggable-authentication.html
 #
-ALTER USER 'ecommerceapp'@'localhost' IDENTIFIED WITH mysql_native_password BY 'ecommerceapp';
+ALTER USER 'ecommerceapp'@'localhost' IDENTIFIED WITH mysql_native_password BY 'ecommerceapp';product
---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/rian/ecommerce/entity/Country.java
@@ -0,0 +1,29 @@
+package com.rian.ecommerce.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name="country")
+@Getter
+@Setter
+
+public class Country {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int Id;
+
+    @Column(name="code")
+    private String code;
+
+    @Column(name="name")
+    private String name;
+
+    @OneToMany(mappedBy = "country")
+    private List<State> states;
+
+}
