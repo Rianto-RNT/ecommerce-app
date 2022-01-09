9d8c3562d019bc2f24835a7168fe790ac15be1a5
Rianto <rianto.rnt@gmail.com>
2022-01-09 09:08:34 +0800

    Checkout Form - Display Validation Error Messages (Frontend)

--- 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -88,6 +88,10 @@ export class CheckoutComponent implements OnInit {
     );
   }
 
+  get firstName() { return this.checkoutFormGroup.get('customer.firstName'); }
+  get lastName() { return this.checkoutFormGroup.get('customer.lastName'); }
+  get email() { return this.checkoutFormGroup.get('customer.email'); }
+
   copyShippingAddressToBillingAddress(event) {
 
     if (event.target.checked) {
