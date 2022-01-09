5f26361d003d9502ca2e3c9a4146179e58ac24b6
Rianto <rianto.rnt@gmail.com>
2022-01-09 10:34:32 +0800

    Checkout Form - Shipping Validation - part 1 (Frontend)

--- 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -47,11 +47,20 @@ export class CheckoutComponent implements OnInit {
                                 Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')])
       }),
       shippingAddress: this.formBuilder.group({
-        street: [''],
-        city: [''],
-        state: [''],
-        country: [''],
-        zipCode: ['']
+        street:new FormControl('',
+                                [Validators.required,
+                                Validators.minLength(1),
+                                ShopRntValidators.notOnlyWhitespace]),
+        city:new FormControl('',
+                              [Validators.required,
+                              Validators.minLength(1),
+                              ShopRntValidators.notOnlyWhitespace]),
+        state: new FormControl('',[Validators.required]),
+        country: new FormControl('',[Validators.required]),
+        zipCode: new FormControl('',
+                                  [Validators.required,
+                                  Validators.minLength(1),
+                                  ShopRntValidators.notOnlyWhitespace])
       }),
       billingAddress: this.formBuilder.group({
         street: [''],
@@ -101,6 +110,13 @@ export class CheckoutComponent implements OnInit {
   get firstName() { return this.checkoutFormGroup.get('customer.firstName'); }
   get lastName() { return this.checkoutFormGroup.get('customer.lastName'); }
   get email() { return this.checkoutFormGroup.get('customer.email'); }
+
+  get shippingAddressStreet() { return this.checkoutFormGroup.get('shippingAddress.street'); }
+  get shippingAddressCity() { return this.checkoutFormGroup.get('shippingAddress.city'); }
+  get shippingAddressState() { return this.checkoutFormGroup.get('shippingAddress.state'); }
+  get shippingAddressZipCode() { return this.checkoutFormGroup.get('shippingAddress.zipCode'); }
+  get shippingAddressCountry() { return this.checkoutFormGroup.get('shippingAddress.country'); }
+
 
   copyShippingAddressToBillingAddress(event) {
 
