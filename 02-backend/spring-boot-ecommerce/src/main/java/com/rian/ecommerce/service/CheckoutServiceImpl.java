f741f2d4c46cc2430783ed11f9d1bcf98accf1c8
Rianto <rianto.rnt@gmail.com>
2022-01-10 20:11:37 +0800

    Checkout Form - save the Order to database - Repo and service pt2  (Backend)

--- 02-backend/spring-boot-ecommerce/src/main/java/com/rian/ecommerce/service/CheckoutServiceImpl.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/rian/ecommerce/service/CheckoutServiceImpl.java
@@ -3,7 +3,9 @@ package com.rian.ecommerce.service;
 import com.rian.ecommerce.dao.CustomerRepository;
 import com.rian.ecommerce.dto.Purchase;
 import com.rian.ecommerce.dto.PurchaseResponse;
+import com.rian.ecommerce.entity.Order;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
  public class CheckoutServiceImpl implements CheckoutService {
@@ -15,7 +17,29 @@ import org.springframework.stereotype.Service;
     }
 
     @Override
+    @Transactional
     public PurchaseResponse placeOrder(Purchase purchase) {
+
+        // retrieve the order info from dto
+        Order order = purchase.getOrder();
+
+        // generate tracking number
+        String orderTrackingNumber = generateOrderTrackingNumber();
+        order.setOrderTrackingNumber(orderTrackingNumber);
+
+        // populate order with orderItem
+
+        // populate order with billingAddress and shippingAddress
+        // populate customer with order
+        // save to database
+        // return a response
         return null;
+    }
+
+    private String generateOrderTrackingNumber() {
+
+        // generate a random UUID number (UUID v4)
+        // For detail see: wikipedia keyword: Universally unique identifier
+        //
     }
 }
