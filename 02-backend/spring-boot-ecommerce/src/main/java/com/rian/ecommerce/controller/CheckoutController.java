379d4d24f410bc5a39d66e021592b7661c38be00
Rianto <rianto.rnt@gmail.com>
2022-01-16 10:28:05 +0800

    checkout form - Save the order to Database - REST Controller + Update spring version (backend)

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/rian/ecommerce/controller/CheckoutController.java
@@ -0,0 +1,30 @@
+package com.rian.ecommerce.controller;
+
+import com.rian.ecommerce.dto.Purchase;
+import com.rian.ecommerce.dto.PurchaseResponse;
+import com.rian.ecommerce.service.CheckoutService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@CrossOrigin("http://localhost:4200")
+@Controller
+@RequestMapping("/api/checkout")
+public class CheckoutController {
+
+    private CheckoutService checkoutService;
+
+    public CheckoutController(CheckoutService checkoutService) {
+        this.checkoutService = checkoutService;
+    }
+
+    @PostMapping("/purchase")
+    public PurchaseResponse placeOrder(@RequestBody Purchase purchase) {
+        PurchaseResponse purchaseResponse = checkoutService.placeOrder(purchase);
+
+        return purchaseResponse;
+    }
+
+}
