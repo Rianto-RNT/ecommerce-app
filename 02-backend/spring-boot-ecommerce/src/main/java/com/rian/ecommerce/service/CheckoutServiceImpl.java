6c33f830456858e623ca8975849e753bd2e5e776
Rianto <rianto.rnt@gmail.com>
2022-01-16 10:07:58 +0800

    checkout form - Save the order to Database - Repo+Service pt3(backend)

--- 02-backend/spring-boot-ecommerce/src/main/java/com/rian/ecommerce/service/CheckoutServiceImpl.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/rian/ecommerce/service/CheckoutServiceImpl.java
@@ -3,9 +3,14 @@ package com.rian.ecommerce.service;
 import com.rian.ecommerce.dao.CustomerRepository;
 import com.rian.ecommerce.dto.Purchase;
 import com.rian.ecommerce.dto.PurchaseResponse;
+import com.rian.ecommerce.entity.Customer;
 import com.rian.ecommerce.entity.Order;
+import com.rian.ecommerce.entity.OrderItem;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Set;
+import java.util.UUID;
 
 @Service
  public class CheckoutServiceImpl implements CheckoutService {
@@ -28,12 +33,22 @@ import org.springframework.transaction.annotation.Transactional;
         order.setOrderTrackingNumber(orderTrackingNumber);
 
         // populate order with orderItem
+        Set<OrderItem> orderItems = purchase.getOrderItems();
+        orderItems.forEach(item -> order.add(item));
 
         // populate order with billingAddress and shippingAddress
+        order.setBillingAddress(purchase.getBillingAddress());
+        order.setShippingAddress(purchase.getShippingAddress());
+
         // populate customer with order
+        Customer customer = purchase.getCustomer();
+        customer.add(order);
+
         // save to database
+        customerRepository.save(customer);
+
         // return a response
-        return null;
+        return new PurchaseResponse(orderTrackingNumber);
     }
 
     private String generateOrderTrackingNumber() {
@@ -41,5 +56,6 @@ import org.springframework.transaction.annotation.Transactional;
         // generate a random UUID number (UUID v4)
         // For detail see: wikipedia keyword: Universally unique identifier
         //
+        return UUID.randomUUID().toString();
     }
 }
