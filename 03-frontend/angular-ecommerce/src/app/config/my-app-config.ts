4a4ef2971e6d09699ef39dd651a596b51c9f6c1b
Rianto <rianto.rnt@gmail.com>
2022-01-18 09:28:46 +0800

    Login Logout - Develop app configuration - Okta (frontend)

---
+++ 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,9 @@
+export default {
+
+  oidc: {
+    clientId: '0oa3mak51jCse7Nbi5d7',
+    issuer: 'https://dev-03701950.okta.com/oauth2/default',
+    redirectUri: 'http://localhost:4200/login/callback',
+    scopes: ['openid', 'profile', 'email']
+  }
+}
