08dd09c321f802b6a3ec3f3c3d0fbc5a2a37fb00
Rianto <rianto.rnt@gmail.com>
2022-01-18 09:53:48 +0800

    Login Logout - Create the login Component (frontend)

---
+++ 03-frontend/angular-ecommerce/src/app/components/login/login.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { OktaAuthStateService } from '@okta/okta-angular';
+import * as OktaSignIn from '@okta/okta-signin-widget'
+
+import myAppConfig from 'src/app/config/my-app-config';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  oktaSignin: any;
+
+  constructor(private oktaAuthService: OktaAuthStateService) {
+
+    this.oktaSignin = new OktaSignIn({
+      logo: 'assets/images/logo.png',
+      baseUrl: myAppConfig.oidc.issuer.split('/oauth2')[0],
+      clientId: myAppConfig.oidc.clientId,
+      redirectUri: myAppConfig.oidc.redirectUri,
+      authParams: {
+        pkce: true,
+        issuer: myAppConfig.oidc.issuer,
+        scopes: myAppConfig.oidc.scopes
+      }
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+}
