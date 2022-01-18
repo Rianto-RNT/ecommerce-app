063011fb05cefb778d23756e21ad9259e9751b5d
Rianto <rianto.rnt@gmail.com>
2022-01-18 10:18:05 +0800

    Login Logout - Develop login status component (frontend)

---
+++ 03-frontend/angular-ecommerce/src/app/components/login-status/login-status.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { OktaAuthService } from '@okta/okta-angular';
+
+@Component({
+  selector: 'app-login-status',
+  templateUrl: './login-status.component.html',
+  styleUrls: ['./login-status.component.css']
+})
+export class LoginStatusComponent implements OnInit {
+
+  isAuthenticated: boolean = false;
+  userFullName: string
+
+  constructor( private oktaAuthService: OktaAuthService) { }
+
+  ngOnInit(): void {
+    // Subscribe to authentication state changes
+    this.oktaAuthService.$authenticationState.subscribe(
+      (result) => {
+        this.isAuthenticated = result;
+        this.getUserDetails();
+      }
+    );
+  }
+
+  getUserDetails() {
+    if (this.isAuthenticated) {
+      // Fetch the logged in user detail (user's claims)
+      //
+      // user full name is exposed as property name
+      this.oktaAuthService.getUser().then(
+        (res)  => {
+          this.userFullName = res.name;
+        }
+      );
+    }
+  }
+
+  logout() {
+    // terminates the session with okta and remove current tokens
+    this.oktaAuthService.signOut();
+  }
+}
